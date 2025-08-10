9f5c5222eb559ec7ce1fa73985a54b3041e653ae
lowbrain <83059975+lowbrain@users.noreply.github.com>
2025-08-10 11:57:43 +0000

    test

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -12,6 +12,7 @@ try {
     $tokenAuthURI = $env:IDENTITY_ENDPOINT + "?resource=$resourceURI&api-version=2019-08-01"
     $tokenResponse = Invoke-RestMethod -Method Get -Headers @{"X-IDENTITY-HEADER"="$env:IDENTITY_HEADER"} -Uri $tokenAuthURI
     $accessToken = $tokenResponse.access_token
+    $groupId = "b52a45f4-e990-403e-84f4-1c4a12e35093"
 
     Write-Host "Successfully acquired access token."
 
@@ -23,17 +24,13 @@ try {
     $logAnalyticsUri = $Request.Body.data.alertContext.condition.allOf.linkToSearchResultsAPI
 
     Write-Host "Querying Log Analytics API at $logAnalyticsUri"
-
     $apiResponse = Invoke-RestMethod -Method GET -Headers $apiRequestHeader -Uri $logAnalyticsUri
-    for ($i = 0; $i -lt $apiResponse.tables[0].rows.Count; $i++) {
-       Write-Host "UserId: $apiResponse.tables.rows[$i][0]"
-    }
 
-    $groupId = "b52a45f4-e990-403e-84f4-1c4a12e35093"
     Connect-MgGraph -Identity
-    $members = Get-MgGroupMember -GroupId $groupId -Filter "id eq"
-    foreach ($member in @($members)) {
-       Write-Host "MemberId: $member"
+    for ($i = 0; $i -lt $apiResponse.tables[0].rows.Count; $i++) {
+        Write-Host "UserId: $apiResponse.tables.rows[$i][0]"
+        $members = Get-MgGroupMember -GroupId $groupId -Filter "id eq $apiResponse.tables.rows[$i][0]"
+        Write-Host "MemberId: $member.value"
     }
 
     $statusCode = [HttpStatusCode]::OK
