f45e51b1766c8f2bd74ef970beea59a4d49e0724
lowbrain <83059975+lowbrain@users.noreply.github.com>
2025-08-10 10:17:49 +0000

    UserIDを出力

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -5,14 +5,6 @@ param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
-Write-Host $Request.Body
-Write-Host $Request.Body.data
-Write-Host $Request.Body.data.alertContext
-Write-Host $Request.Body.data.alertContext.condition
-Write-Host $Request.Body.data.alertContext.condition.allOf
-Write-Host $Request.Body.data.alertContext.condition.allOf.linkToSearchResultsAPI
-Write-Host $env:IDENTITY_ENDPOINT
-Write-Host $env:IDENTITY_HEADER
 
 try {
     # Azure サービスのリソース URI を指定してローカル エンドポイントからトークンを取得
@@ -33,17 +25,20 @@ try {
     Write-Host "Querying Log Analytics API at $logAnalyticsUri"
 
     $apiResponse = Invoke-RestMethod -Method GET -Headers $apiRequestHeader -Uri $logAnalyticsUri 
-    $body = $apiResponse | ConvertTo-Json -Depth 10
+    $apiResponse. tables.rows | ForEach-Object {
+       Write-Host "UserId: $_[0]"
+    }
+
     $statusCode = [HttpStatusCode]::OK
 }
 catch {
     Write-Error $_.Exception.Message
-    $body = $_.Exception.Message
     $statusCode = [HttpStatusCode]::InternalServerError
 }
+
+Write-Host "Result: $body"
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $statusCode
-    Body = $body
 })
