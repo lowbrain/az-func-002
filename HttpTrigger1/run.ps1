03ea6a9663f21e778a531640adc314c2d5a3e395
lowbrain <83059975+lowbrain@users.noreply.github.com>
2025-08-10 13:07:13 +0000

    もう一回

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -28,11 +28,9 @@ try {
 
     Connect-MgGraph -Identity
     for ($i = 0; $i -lt $apiResponse.tables[0].rows.Count; $i++) {
-        $userId = [guid]$apiResponse.tables[0].rows[$i][0]
+        $userId = $apiResponse.tables[0].rows[$i][0]
         Write-Host "UserId: $userId"
-        $filter = "id eq guid'$userId'"
-        Write-Host "filter: $filter"
-        $members = Get-MgGroupMember -GroupId $groupId -Filter $filter
+        $members = Get-MgGroupMember -GroupId $groupId -Filter "Id eq '$userId'"
         Write-Host "MemberId: $members.value"
     }
 
